56b24e065077bfd9e157ad015e142b730da5daea
Nikita Singh <nikitasingh.1022.com>
2023-04-16 00:42:41 +0530

    socialLinksLinked

--- src/components/Widget/SocialWidget.jsx
+++ src/components/Widget/SocialWidget.jsx
@@ -1,23 +1,35 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import { Icon } from '@iconify/react';
 import Div from '../Div';
 
 export default function SocialWidget() {
   return (
     <Div className="cs-social_btns cs-style1">
-      <Link to='/' className="cs-center">
-        <Icon icon="fa6-brands:linkedin-in" />
-      </Link>
-      <Link to='/' className="cs-center">
+      {/* <Link to='/' className="cs-center">
+      </Link> */}
+      <a href="https://www.facebook.com/HastaLaVista.cse.tmsl" className="cs-center">
+        <Icon icon="fa6-brands:facebook" />
+      </a>
+      {/* <Link to='/' className="cs-center">
         <Icon icon="fa6-brands:twitter" />               
-      </Link>
-      <Link to='/' className="cs-center">
+      </Link> */}
+      <a href="https://www.youtube.com/@HastaLaVista_cse" className="cs-center">
         <Icon icon="fa6-brands:youtube" />              
-      </Link>
-      <Link to='/' className="cs-center">
-        <Icon icon="fa6-brands:slack" />
-      </Link>
+      </a>
+      {/* <Link to='/' className="cs-center">
+      </Link> */}
+      {/* <Link to='/' className="cs-center">
+        <Icon icon="fa6-brands:youtube" />              
+      </Link> */}
+      <a href="https://www.instagram.com/hasta_la_vista.cse/" className="cs-center">            
+        <Icon icon="fa6-brands:instagram" />
+      </a>
+      <a href="mailto:hastalavista.cse.tmsl@gmail.com" className="cs-center">            
+        <Icon icon="ph:envelope-open-fill" />
+      </a>
+      {/* <Link to='/' className="cs-center">
+      </Link> */}
     </Div>
   )
 }
