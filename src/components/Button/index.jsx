e7789d0e6d9cd01b52d9d4ee5d741633195549cb
Sabyasachi <yoboy907@gmail.com>
2023-04-17 15:29:57 +0530

    Footer Link Fixed

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 
 export default function Button({ btnLink, btnText, variant, icon }) {
   return (
-    <Link
-      to={btnLink}
+    <a
+      href={btnLink}
       className={variant ? `cs-text_btn ${variant}` : 'cs-text_btn'}
     >
       <>
         <span>{btnText}</span>
         {icon ? icon : <Icon icon="bi:arrow-right" />}
       </>
-    </Link>
+    </a>
   );
 }
