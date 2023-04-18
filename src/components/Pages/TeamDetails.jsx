ef37f902c48784dfbe51ac22ed260ff1c1bc1622
Nikita Singh <nikitasingh.1022.com>
2023-04-18 14:56:02 +0530

    teacherPageAdded

--- src/components/Pages/TeamDetails.jsx
+++ src/components/Pages/TeamDetails.jsx
@@ -5,6 +5,7 @@ import PageHeading from '../PageHeading'
 import Div from '../Div'
 import Spacing from '../Spacing'
 import SocialWidget from '../Widget/SocialWidget'
+import teamDetailscss from './teamDetailsscss.scss'
 
 export default function TeamDetails() {
   pageTitle('Team Member');
@@ -24,9 +25,20 @@ export default function TeamDetails() {
           <Div className="col-xl-5 col-lg-6">
             <Div className="cs-radius_15 cs-shine_hover_1">
               <img src="/images/member_details_1.jpeg" alt="Member" className="w-100" />
+              <h2 className="cs-section_title section">Moutushi Banerjee</h2>
+              <Div className="cs-height_10 cs-height_lg_10" />
+              <h3 className="cs-section_subtitle section">Teacher Co-Ordinator</h3>
             </Div>
           </Div>
-          <Div className="col-lg-6 offset-xl-1">
+          <Div className="col-xl-5 col-lg-6">
+            <Div className="cs-radius_15 cs-shine_hover_1">
+              <img src="/images/member_details_1.jpeg" alt="Member" className="w-100" />
+              <h2 className="cs-section_title section">Mrinal Kanti Nath</h2>
+              <Div className="cs-height_10 cs-height_lg_10" />
+              <h3 className="cs-section_subtitle section">Teacher Co-Ordinator</h3>
+            </Div>
+          </Div>
+          {/* <Div className="col-lg-6 offset-xl-1">
             <Spacing lg='0' md='45'/>
             <Div className="cs-section_heading cs-style1">
               <h2 className="cs-section_title">Melon Bulgery</h2>
@@ -41,7 +53,7 @@ export default function TeamDetails() {
               <Div className="cs-height_45 cs-height_lg_30" />
               <SocialWidget/>
             </Div>
-          </Div>
+          </Div> */}
         </Div>
       </Div>
       <Spacing lg='150' md='80'/>
